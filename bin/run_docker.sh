e617b73a05b2a5015bc637200ddeb24eee150ae9
Fester Herenius <me@festerherenius.nl>
2024-04-07 22:53:34 +0200

    Refactor: Format run_docker.sh

--- bin/run_docker.sh
+++ bin/run_docker.sh
@@ -1,4 +1,8 @@
 #/bin/bash
 
 # Run the docker container
-docker run --rm -p 127.0.0.1:5000:5000 --env APP_SECRET_KEY=192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf local/app:latest
+docker run \
+    --rm \
+    -p 127.0.0.1:5000:5000 \
+    --env APP_SECRET_KEY=192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf \
+    local/app:latest
